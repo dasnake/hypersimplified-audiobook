2e5fea14843a13dc5d2905f273addf642119e1b7
Oracle Public Cloud User <opc@instance-20220205-2225.subnet02052239.vcn02052239.oraclevcn.com>
2024-05-22 20:45:43 +0000

    fix

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", async function() {
     const audio = document.getElementById('audioPlayer');
     const playPauseButton = document.getElementById('playButton');
     const forwardButton = document.getElementById('forwardButton');
@@ -32,84 +32,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const MIN_BUFFER_THRESHOLD = 10;
 
-    function arrayBufferToMd5(arrayBuffer) {
-	// Convert ArrayBuffer to WordArray first
-	var wordArray = CryptoJS.lib.WordArray.create(arrayBuffer);
+
+    fetchFileSize().then(size => {
+	totalSize = size; 
+	playPauseButton.textContent = calculatePercentage(totalSize);
+    });
     
-	// Compute MD5 hash
-	var hash = CryptoJS.MD5(wordArray);
-    
-	// Convert the hash to a hexadecimal string
-	return hash.toString(CryptoJS.enc.Hex);
-    }
-    
-    function getClientId() {
-        let clientId = localStorage.getItem("clientId");
-        if (!clientId) {
-            clientId = Math.floor(Math.random() * 10000).toString();
-            localStorage.setItem("clientId", clientId);
-        }
-        return clientId;
-    }
-
-    // Function to fetch the file size from the FastAPI endpoint
-    function fetchFileSize() {
-	fetch(`https://${location.host}/size`)
-	    .then(response => {
-		if (!response.ok) {
-		    throw new Error('Network response was not ok');
-		}
-		return response.json();
-	    })
-	    .then(data => {
-		if (data.error) {
-		    console.error('Error fetching file size:', data.error);
-		} else {
-		    console.log("File size", data.file_size);
-		    totalSize = data.file_size;
-		    playPauseButton.textContent = calculatePercentage();
-		    return data.file_size;
-		}
-	    })
-	    .catch(error => {
-		console.error('There was a problem with your fetch operation:', error);
-	    });
-    }    
-
-    fetchFileSize();
-
-    // Function to calculate the percentage
-    function calculatePercentage() {
-	const position = getCookie('last_position');
-	if (position) {
-	    console.log("Current position", position);
-	    console.log("Max position", totalSize);
-	    const percentage = (position / totalSize) * 100;
-	    console.log(`Percentage: ${percentage.toFixed(2)}%`);
-	    return percentage.toFixed(2);
-	} else {
-	    console.log("Cookie 'position' not found.");
-	    return 0;
-	}
-    }
-    
-    function getCookie(name) {
-	let cookieArray = document.cookie.split(';'); // Split document.cookie at each semicolon (which separates cookies)
-	for (let cookie of cookieArray) {
-            let [cookieName, cookieValue] = cookie.trim().split('='); // Split each individual cookie into name and value at the equal sign
-            if (cookieName === name) {
-		return cookieValue; // Return the value if the names match
-            }
-	}
-	return null; // Return null if the cookie was not found
-    }
-
-    function updateCookie(name, value) {
-	const daysToExpire = 7; // Number of days until the cookie should expire
-	let expires = new Date(Date.now() + daysToExpire * 86400000).toUTCString(); // Calculate expiration date
-	document.cookie = `${name}=${value}; expires=${expires}; path=/`; // Set the cookie with expiration and path
-    }
-
     function checkBufferHealth() {
         if (mediaSource.readyState === 'open' && !sourceBuffer.updating) {
             let bufferEnd = sourceBuffer.buffered.length > 0 ? sourceBuffer.buffered.end(0) : 0;
